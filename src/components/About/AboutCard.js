d8eb216ddf92ba2b31aca05820a145810740d4c9
Aman Kumar Gupta <75511608+codecraft26@users.noreply.github.com>
2023-12-05 14:39:26 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Aman Kumar Gupta </span>
-            from <span className="purple"> Deoria UttarPradesh, India.</span>
+            from <span className="purple"> Delhi, India.</span>
             <br />I am Full Stack developer @Infinite Computer Solution
             <br />
             <br />
